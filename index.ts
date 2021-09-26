9cc79172b7a1be1e7cd23b6e4952ef11fbe05708
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-09-26 10:50:13 -0700

    type definitions and beginning of searchEvents fxn

---
+++ index.ts
@@ -0,0 +1,27 @@
+import courses from './courses';
+import studyGroups from './studyGroups';
+
+type Course = {
+    id: number;
+    studyGroupId: number;
+    title: string;
+    keywords: string[];
+    eventType: string;
+}
+
+type StudyGroup = {
+    id: number;
+    courseId: number;
+    title: string;
+    keywords: string[];
+    eventType: string;
+}
+
+type SearchEventsOptions = {
+    query: number | string;
+    eventType: 'courses' | 'groups';
+}
+
+function searchEvents(options: SearchEventsOptions) {
+
+}
